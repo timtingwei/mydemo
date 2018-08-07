cddc2c0be909130e0893f7b8d69a1e799e35fc03
timtingwei <timtingwei@hotmail.com>
2018-08-08 00:07:59 +0800

    测试log

---
+++ TestLog.java
@@ -0,0 +1,9 @@
+
+public class TestLog {
+  public static void main(String[] args) {
+    double d1 = Math.log(100);     // 以e为底
+    double d2 = Math.log10(100);   // 以10为底
+    System.out.println(d1);
+    System.out.println(d2);
+  }
+}
