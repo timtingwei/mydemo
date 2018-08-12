e2b41c987a708daabe76a07ad79ddced29059d95
timtingwei <timtingwei@hotmail.com>
2018-08-13 00:10:43 +0800

    使用指针而不是引用

--- vectorPush.cpp
+++ vectorPush.cpp
@@ -10,12 +10,16 @@ void udef(vector<int> q, int n) {
   q.push_back(n);
 }
 
+void pfoo(vector<int> *pvec, int n) {   // 建议使用指针而不是引用 Effective C++
+  pvec->push_back(n);
+}
 
 int main() {
   vector<int> q;
   // udef(q, 5);
   rdef(q, 5);
 
+  pfoo(&q, 5);
 
   std::cout << q.size() << " " << *(q.begin()) << std::endl;    // 1  5
   return 0;
