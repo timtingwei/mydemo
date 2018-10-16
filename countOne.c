6386c99a0dabef525c3a7027793c0c4ff234aca4
timtingwei <timtingwei@hotmail.com>
2018-10-17 03:46:12 +0800

    Create CountOne.c

---
+++ countOne.c
@@ -0,0 +1,16 @@
+// Copyright [2018] <mituh>
+#include <stdio.h>
+#include <string.h>
+#define maxn 1000 + 10
+int main() {
+  char s[maxn];
+  scanf("%s", s);
+  // /*
+  int tot = 0;
+  for (int i = 0; i < strlen(s); i++)
+    if (s[i] == '1') tot++;
+  printf("%d\n", tot);
+  // */
+  return 0;
+}
+
